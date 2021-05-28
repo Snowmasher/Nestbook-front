c1ae6cbcaaf2e270998e88b8eb2041c79ba4f33e
Snowmasher <alvaroroblesortega@gmail.com>
2021-05-28 05:26:02 +0200

    Ultimos cambios

--- src/app/GrupoGeneral/info-bar/info-bar.component.ts
+++ src/app/GrupoGeneral/info-bar/info-bar.component.ts
@@ -8,44 +8,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./info-bar.component.css']
 })
 export class InfoBarComponent implements OnInit {
-  info: any;
-  idAsoc!: number;
   asociacion: any;
   url = 'http://localhost:8000/api/asociacion/';
-  constructor(private service: AsociacionService, private http: HttpClient,) { }
+  constructor(
+    private service: AsociacionService,
+    private http: HttpClient
+    ) { }
 
   async ngOnInit(){
-
-    //this.asociacion = await this.service.getAsocInfoBar();
-    let res: any;
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('token')}`
-    });
-    this.http.get('http://localhost:8000/api/user', { headers }).toPromise().then(
-      result => {
-        console.log(result);
-        res = result;
-        this.asociacion = res.id_asociacion;
-      },
-      error => {
-        console.log(error);
-      }
-    ).finally(
-      () => {
-        this.url = this.url + this.asociacion;
-        this.http.get(this.url).toPromise().then(
+        this.http.get(this.url + localStorage.getItem('id_asociacion')).subscribe(
           (      result: any) => {
             console.log(result);
-            this.asociacion = result;
+            this.asociacion = result[0];
           },
           (      error: any) => {
             console.log(error);
           }
-        ).finally(
-          this.asociacion = this.info
-        );
+        )
       }
-    );
-  }
   }
 
--- src/app/GrupoPrincipal/lista-miembros/lista-miembros.component.ts
+++ src/app/GrupoPrincipal/lista-miembros/lista-miembros.component.ts
@@ -14,7 +14,7 @@ export class ListaMiembrosComponent implements OnInit {
   constructor(private http: HttpClient, private service: UserService) { }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:8000/api/usersByAsoc/1').subscribe(
+    this.http.get('http://localhost:8000/api/usersByAsoc/' + localStorage.getItem('id_asociacion')).subscribe(
       (result: any) => {
         this.miembros = result;
       },
